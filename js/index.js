891a91c85aa645b6f28713948053a92faffbb393
XinshenXiangChen <xinshenxiangchen@gmail.com>
2025-02-11 18:36:55 +0100

    new items

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 500;
+
+context.fillRect(0, 0, canvas.width, canvas.height);
